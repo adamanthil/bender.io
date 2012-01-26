23d4a067aaa6dc3f40168ecb21d1e44df4fdc663
adamanthil <andrew@bender.io>
2012-01-26 00:58:58 -0600

    updating rotation javascript to shuffle better

--- scripts/site.js
+++ scripts/site.js
@@ -1,19 +1,32 @@
-$(document).ready(function() {
-    var primaryTitles = ['entrepreneur', 'computer scientist'];
-    var titles = ['videographer', 'pseudo-designer', 'product manager', 
-      'second bass', 'tea-drinker', 'violinist', 'ballroom dancer', 'software developer', 'movie-maker'];
+shuffle = function(o) {
+  for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
+};
 
+range = function (start, end) {
+    var array = [];
+    for (var i = start; i <= end; i++)
+        array.push(i);
+    return array;
+};
+
+
+$(document).ready(function() {
     var titles = $('#header .titles');
     var activeTitle = $('#header .title.active');
+    var titleList = titles.children('.title');
+    var titleOrder = shuffle(range(0, titleList.length - 1));
+    var activeIndex = 0;
 
     var switchTitle = function() {
-      var titleList = titles.children('.title');
-      var titleIndex = Math.floor(Math.random() * titleList.length);
+      var titleIndex = titleOrder[activeIndex];
       var title = $(titleList[titleIndex]);
       
       activeTitle.fadeToggle(600);
       title.fadeToggle(600);
       activeTitle = title;
+      
+      activeIndex = activeIndex >= titleList.length - 1 ? 0 : activeIndex + 1;
     };
     
     setInterval(switchTitle, 3200);
