997365c5f907c861b417a35f21862032171fd592
Andrew Bender <andrew@bender.io>
2013-09-02 15:49:35 -0500

    simplify & correct twitter proxy output

--- twitterproxy/get-tweets.php
+++ twitterproxy/get-tweets.php
@@ -4,15 +4,35 @@
 require_once ('codebird/src/codebird.php');
 require_once ('credentials.php');
 
-//Get authenticated
+// Get authenticated
 \Codebird\Codebird::setConsumerKey(CONSUMER_KEY, CONSUMER_SECRET);
 $cb = \Codebird\Codebird::getInstance();
 $cb->setToken(ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 
-//Make the REST call
-$data = (array) $cb->statuses_homeTimeline();
+// Make the REST call
+$params = array(
+	'screen_name' => 'adamanthil',
+	'count' => 5,
+	'exclude_replies' => true
+);
 
-//Output result in JSON, getting it ready for jQuery to process
-echo json_encode($data);
+$data = (array) $cb->statuses_userTimeline($params);
+
+$output = array();
+$count = 0;
+foreach($data as $tweet) {
+	$output[] = array(
+		'tweet' => $tweet['text'],
+		'time' => $tweet['created_at']
+	);
+	$count++;
+
+	// Twitter's API doesn't always return the full count
+	// so make sure we only get 2 tweets
+	if($count > 1) break;
+}
+
+// Output result in JSON, getting it ready for jQuery to process
+echo json_encode($output);
 
 ?>
