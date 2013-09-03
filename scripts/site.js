8d81f9e959bfa26244a537a7359f589b4fc03a7e
Andrew Bender <andrew@bender.io>
2013-09-02 19:33:57 -0500

    add twitter feed ajax

--- scripts/site.js
+++ scripts/site.js
@@ -1,34 +1,16 @@
-shuffle = function(o) {
-  for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-  return o;
-};
-
-range = function (start, end) {
-    var array = [];
-    for (var i = start; i <= end; i++)
-        array.push(i);
-    return array;
-};
-
-
 $(document).ready(function() {
-    var titles = $('#header .titles');
-    var activeTitle = $('#header .title.active');
-    var titleList = titles.children('.title');
-    var titleOrder = shuffle(range(0, titleList.length - 1));
-    var activeIndex = 0;
 
-    var switchTitle = function() {
-      var titleIndex = titleOrder[activeIndex];
-      var title = $(titleList[titleIndex]);
-
-      activeTitle.fadeToggle(600);
-      title.fadeToggle(600);
-      activeTitle = title;
-
-      activeIndex = activeIndex >= titleList.length - 1 ? 0 : activeIndex + 1;
-    };
-
-    setInterval(switchTitle, 3200);
+  $.ajax({
+    url: 'twitter-proxy/get-tweets.php?screen_name=adamanthil&count=3'
+  }).success(function(data) {
+    if(data && data.length) {
+      var html = '<ul class="tweet_list">';
+      for(var i = 0; i < data.length; i++) {
+        html += '<li><div class="tweet">' + data[i].tweet_html + '</div><div class="tweet-time">' + data[i].time + '</div></li>';
+      }
+      html += '</ul>';
+      $('#tweets').html(html);
+    }
+  });
 
  });
